0e3ec399494c591f801bc79d4cb72460569ca468
Natrajan Jaiswal <natrajan.jaiswal@natrajan-mac.local>
2020-06-07 20:14:25 +0530

    add

--- contact.php
+++ contact.php
@@ -1,45 +1,6 @@
 <!DOCTYPE html>
 
-<?php
-    if (isset($_POST['submit'])){
 
-require_once('PHPmailer/PHPMailerAutoload.php');
-
-$first_name=$_POST["first_name"];
-$last_name=$_POST["last_name"];
-$email=$_POST["email"];
-$phone=$_POST["phone_no"];
-$message=$_POST["con_message"];
-
-
-
-$mail=new PHPMailer();
-$mail->isSMTP();
-$mail->SMTPAuth='true';
-$mail->SMTPSecure='ssl';
-$mail->Host="smtp.gmail.com";
-//$mail->Host="smtp.hostinger.in";
-
-$mail->Port='465';
-$mail->isHTML();
-$mail->Username="socialplayerdotin@gmail.com";
-$mail->Password='natrajan807';
-$mail->SetFrom('hello@socialplayer.in');
-$mail->AddReplyTo('hello@socialplayer.in');
-$mail->FromName='Priyanka Singh';
-$mail->Subject="Welcome to Socialplayer";
-$mail->Body= "Name: $first_name + $last_name + $phone + $email + $message";
-$mail->addAddress('njaiswal78@gmail.com');
-if(!$mail->Send())
-{
-   echo "Error sending: " . $mail->ErrorInfo;
-}
-else
-{
-   echo "E-mail sent";
-}
-}
-?>
 
 <html class="no-js" lang="zxx">
 
